0c3cdc7b182a1e68554045ab6ca7c77ac37aa9fa
Josh <joshisadesigner@gmail.com>
2018-10-17 09:42:24 -0600

    Initial commit

---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('MenuCtrl', ['foodFinder', function (menu) {
+      var vm = this;
+
+      menu.getMenu().then(function(data) {
+        vm.items = data;
+      });
+
+      this.increment = function (item) {
+        // item.rating += 0.1;
+        item.rating = ((item.rating * 10) + 1) / 10;
+      };
+      this.decrement = function (item) {
+        // item.rating -= 0.1;
+        item.rating = ((item.rating * 10) - 1) / 10;
+      };
+    }
+  ]);
